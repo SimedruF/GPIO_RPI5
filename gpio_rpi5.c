5df518d4cdf3d202d56d9718cd348ac3f69a5c26
SimedruF <simedruflorin@yahoo.com>
2024-06-25 09:33:33 +0300

    Simple GPIO lib for RPI5

---
+++ gpio_rpi5.c
@@ -0,0 +1,132 @@
+// Compile with $ gcc gpio_rpi5.c -o gpio_rpi5
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <cstring>
+#include "gpio_rpi5.h"
+
+// Pin modes:
+#define INPUT (0)
+#define OUTPUT (1)
+#define LOW (0)
+#define HIGH (1)
+#define UNDEF (3)
+#define TEST_PIN 17
+pin_t rpi5_gpio[237];
+
+int gpio_pintest(int pin_indx)
+{
+  int time = 0;
+  pin_t testpin = pinopen(pin_indx, OUTPUT);
+  pinwrite(pin_indx, HIGH);
+  sleep(1000);
+  pinwrite(pin_indx, LOW);
+  pinclose(pin_indx);
+  return 0;
+}
+
+pin_t pinopen(int pin, int mode)
+{
+   char *cmd_openPin = (char*)malloc(1024);
+   char*   pinfn = (char*)malloc(1024);
+   FILE *pf;
+   char data[512];
+   if(mode == OUTPUT)
+   {
+     snprintf(cmd_openPin, 1024, "pinctrl set %d op", pin);
+   }
+   else
+   {
+      snprintf(cmd_openPin, 1024, "pinctrl set %d ip", pin);
+   }
+
+   pf = popen(cmd_openPin,"r"); 
+   if(!pf)
+   {
+     fprintf(stderr, "Could not open pipe for output.\n");
+     exit(1);
+   }
+
+   if (pclose(pf) != 0)
+       fprintf(stderr," Error: Failed to close command stream \n");
+   
+   rpi5_gpio[pin].mode =  mode;
+   return (pin_t) { UNDEF, mode };
+}
+
+void pinclose(int indx_pin)
+{
+  rpi5_gpio[indx_pin].mode =  UNDEF;
+  rpi5_gpio[indx_pin].state =  UNDEF;
+}
+
+void pinwrite(int indx_pin, int value)
+{
+   char *cmd_openPin = (char*)malloc(1024);
+   char*   pinfn = (char*)malloc(1024);
+   if(value == LOW)
+   {
+     snprintf(cmd_openPin, 1024, "pinctrl set %d dl", indx_pin);
+   }
+   else
+   {
+      snprintf(cmd_openPin, 1024, "pinctrl set %d dh", indx_pin);
+   }
+   rpi5_gpio[indx_pin].state =  value;
+   int returnCode = system(cmd_openPin);
+   // checking if the command was executed successfully
+   if (returnCode < 0){
+        printf("Command execution failed or returned "
+               "non-zero: %d", returnCode);
+               exit(1);
+    }
+}
+
+int pinread(int pin)
+{
+  char *cmd_openPin = (char*)malloc(1024);
+  FILE *pf;
+  char data[512];
+  int pinout = 0;
+  char *pin_off = "lo";
+  char *pin_on = "hi";
+  snprintf(cmd_openPin, 1024, "pinctrl get %d ", pin);
+
+  pf = popen(cmd_openPin,"r"); 
+  if(!pf)
+  {
+    fprintf(stderr, "Could not open pipe for output.\n");
+    exit(1);
+  }
+ 
+  // Grab data from process execution
+  fgets(data, 512 , pf);
+  // Print grabbed data to the screen.
+  fprintf(stdout, "-%s-\n",data); 
+ 
+  char *pch_off = strstr(data, pin_off);
+  char *pch_on = strstr(data, pin_on);
+
+  if(pch_off)
+  {
+    fprintf(stdout, "-%s-\n",pch_off); 
+    pinout = 0;
+  }
+  if(pch_on)
+  {
+    fprintf(stdout, "-%s-\n",pch_on); 
+    pinout = 1;
+  }
+  rpi5_gpio[pin].state =  pinout;
+
+  if (pclose(pf) != 0)
+       fprintf(stderr," Error: Failed to close command stream \n");
+
+  return pinout;
+
+}
